ab224cc7aa3f456e544adf757acad6637546cb76
Nakadzima <pascal.german@yandex.ru>
2022-10-10 02:52:14 +0600

    added C# dir

---
+++ lab-Csharp/ConsoleApp/LabOne.cs
@@ -0,0 +1,16 @@
+namespace ConsoleApp;
+using System;
+public static class SumOfNumber
+{
+    public static void Main()
+    {
+        Console.Write("a=");
+        int a = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+        Console.Write("b=");
+        int b = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+        Console.Write("c=");
+        int c = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+        Console.WriteLine("{0}+{1}+{2}={3}", a,b,c, a+b+c);
+    }
+
+}
